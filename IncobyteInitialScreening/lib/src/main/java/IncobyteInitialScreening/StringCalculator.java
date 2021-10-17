eab301bb04f6d59b98acdd4ef3dd45315c155487
LeiziRavi <ravi4582@outlook.com>
2021-10-18 01:36:57 +0530

    Refractor StringCalculator.java
    Refractor StringCalculator.java extracting methods to split the string and converting string array to integer list, leaving only principal add method in public

--- IncobyteInitialScreening/lib/src/main/java/IncobyteInitialScreening/StringCalculator.java
+++ IncobyteInitialScreening/lib/src/main/java/IncobyteInitialScreening/StringCalculator.java
@@ -1,26 +1,27 @@
 package IncobyteInitialScreening;
 
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
-
 
 public class StringCalculator {
 
 	public int add(String string) {
 		if (!string.isEmpty()) {
-			
-			List<Integer> numbers = Arrays.stream(string.split(",|\n"))
-					.map(Integer::parseInt)
-					.collect(Collectors.toList());
-			return numbers.stream()
-					.reduce(Integer::sum)
-					.orElseThrow();
+
+			List<Integer> numbers = stringArraytoIntegerList(getSplit(string));
+			return numbers.stream().reduce(Integer::sum).orElseThrow();
 		}
-		
-		    return 0; 
-		}
+
+		return 0;
+	}
+
+	private List<Integer> stringArraytoIntegerList(String[] stringArray) {
+		return Arrays.stream(stringArray).map(Integer::parseInt).collect(Collectors.toList());
+	}
+
+	private String[] getSplit(String string) {
+		return string.split(",|\n");
+	}
 
 }
