79cbad81cfdefd5ec645fbaee3247469de6a4927
LeiziRavi <ravi4582@outlook.com>
2021-10-18 02:26:50 +0530

    Update StringCalculator.java
    Implementation method to check for negative numbers in input string and throw exception if found any

--- IncobyteInitialScreening/lib/src/main/java/IncobyteInitialScreening/StringCalculator.java
+++ IncobyteInitialScreening/lib/src/main/java/IncobyteInitialScreening/StringCalculator.java
@@ -10,12 +10,21 @@ public class StringCalculator {
 
 	public int add(String string) {
 		if (!string.isEmpty()) {
-
 			List<Integer> numbers = stringArraytoIntegerList(getSplit(string));
+			negatives(numbers);
 			return numbers.stream().reduce(Integer::sum).orElseThrow();
 		}
 
 		return 0;
+	}
+
+	private void negatives(List<Integer> numbers) {
+		StringBuilder stringBuilder = new StringBuilder();
+		numbers.stream().filter(num -> num < 0).forEach(num -> stringBuilder.append(num).append(" "));
+		if (!stringBuilder.toString().isEmpty()) {
+			throw new IllegalArgumentException("Negatives not allowed: " + stringBuilder.toString());
+		}
+
 	}
 
 	private List<Integer> stringArraytoIntegerList(String[] stringArray) {
@@ -23,9 +32,9 @@ public class StringCalculator {
 	}
 
 	private String[] getSplit(String string) {
-		if(string.startsWith("//")) {
-			Matcher matcher= Pattern.compile("//(.)\n(.*)").matcher(string);
-			if(matcher.matches()) {
+		if (string.startsWith("//")) {
+			Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(string);
+			if (matcher.matches()) {
 				String delimiter = matcher.group(1);
 				String toSplit = matcher.group(2);
 				return toSplit.split(delimiter);
